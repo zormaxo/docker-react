6ea67df6694fe8c95161b0a80be4a26a0f0310fd
zormax <okocadere@gmail.com>
2022-10-25 08:37:08 +0300

    changed app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is feature branch</p>
+        <p>This is feature branch for github actions</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
